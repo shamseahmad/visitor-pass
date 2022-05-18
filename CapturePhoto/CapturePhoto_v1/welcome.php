f9b8aa3bfc4d777e2f1e70f8e6f0591f17d4c00e
Shamse Ahmad <80626242+shamseahmad@users.noreply.github.com>
2022-05-18 11:53:41 +0530

    Add files via upload

---
+++ CapturePhoto/CapturePhoto_v1/welcome.php
@@ -0,0 +1,21 @@
+<?php
+	$Name_1 = $_POST['Name_1'];
+	$Phone_no = $_POST['Phone_no'];
+	$address = $_POST['address'];
+	$nameOfPersonToMeet = $_POST['nameOfPersonToMeet'];
+	$Department = $_POST['Department'];
+	$purposeOfVisit = $_POST['purposeOfVisit'];
+
+	// Database connection
+	$conn = new mysqli('localhost','root','','test');
+	if($conn->connect_error){
+		echo "$conn->connect_error";
+		die("Connection Failed : ". $conn->connect_error);
+	} else {
+		$stmt = $conn->prepare("insert into visitorpass(Name_1, Phone_no, address, nameOfPersonToMeet, Department, purposeOfVisit) values(?, ?, ?, ?, ?, ?)");
+		$stmt->bind_param("ssssss", $Name_1, $Phone_no, $address, $nameOfPersonToMeet, $Department, $purposeOfVisit);
+		$execval = $stmt->execute();
+		$stmt->close();
+		$conn->close();
+	}
+?>
